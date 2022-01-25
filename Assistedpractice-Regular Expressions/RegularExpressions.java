5e9e0f747eb5b837e3a3fd7c2dc9ecb4b1bf5ea6
Vaishnavi <vdonade@gmail.com>
2022-01-26 01:36:41 +0530

    Regular Expressions

---
+++ Assistedpractice-Regular Expressions/RegularExpressions.java
@@ -0,0 +1,23 @@
+package com.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularExpressions {
+
+
+	public static void main(String[] args) {
+		
+		String pattern="[a-z]+";
+		
+		Pattern p=Pattern.compile(pattern);
+		String check="Regular Expression";
+		Matcher match= p.matcher(check);
+		
+		while(match.find())
+		{
+			System.out.println(check.substring(match.start(),match.end()));
+		}
+		
+	}
+}
