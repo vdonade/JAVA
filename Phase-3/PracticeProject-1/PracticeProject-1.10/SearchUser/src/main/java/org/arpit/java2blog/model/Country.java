bd41d1ba6d9c959ba49184773d0ad17e75caf7f6
Vaishnavi <vdonade@gmail.com>
2022-02-28 16:39:44 +0530

    Practice project-1

---
+++ Phase-3/PracticeProject-1/PracticeProject-1.10/SearchUser/src/main/java/org/arpit/java2blog/model/Country.java
@@ -0,0 +1,58 @@
+package org.arpit.java2blog.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*
+ * This is our model class and it corresponds to Country table in database
+ */
+@Entity
+@Table(name="Customer")
+public class Country{
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	int id;
+	
+	@Column(name="customerName")
+	String customerName;	
+	
+	@Column(name="email")
+	String email;
+
+	
+	
+	public Country() {
+		super();
+	}
+	public Country(int i, String customerName,String email) {
+		super();
+		this.id = i;
+		this.customerName = customerName;
+		this.email=email;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
---
+++ Phase-3/PracticeProject-1/PracticeProject-1.10/SearchUser/src/main/webapp/error.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>UserId is Invalid!</h2>
+</body>
+</html>
